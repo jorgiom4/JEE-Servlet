8ac6838cd5725d1021c6555bf201c0049be4755f
cice <cice@A8POVMac07.local>
2018-09-17 21:49:42 +0200

    Commit del ejercicio

---
+++ src/main/java/com/instancia2/controllers/CicloVidaServlet.java
@@ -0,0 +1,31 @@
+package com.instancia2.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+
+@WebServlet(name = "CicloVidaServlet", urlPatterns = "/ciclovida")
+public class CicloVidaServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("---- Dentro del init del servlet");
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+        System.out.println("---- Dentro del service del servlet");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("---- Dentro del destroy del servlet");
+    }
+
+}
